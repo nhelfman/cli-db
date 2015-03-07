e6f680d5f698d51f8f5db8e3507ee28be70c3450
Noam Helfman <noam.helfman@gmail.com>
2015-03-06 19:47:17 -0800

    print usage on all arguments error

--- clidb/main.py
+++ clidb/main.py
@@ -3,15 +3,20 @@ import sys
 from cli_db import CliDb
 
 
+def usage():
+    print "Usage: cli-db <set key value|get key|del key>"
+
+
 def main(args):
     if len(args) < 3:
-        print "Usage: cli-db <set key value|get key|del key>"
+        usage()
 
     command = args[1]
     if command == "set":
         key = args[2]
         if len(args) < 4:
             print "missing argument: value"
+            usage()
             return 1
 
         value = args[3]
@@ -22,6 +27,7 @@ def main(args):
     elif command == "get":
         if len(args) < 3:
             print "missing argument: key"
+            usage()
             return 1
 
         key = args[2]
@@ -36,6 +42,7 @@ def main(args):
     elif command == "del":
         if len(args) < 3:
             print "missing argument: key"
+            usage()
             return 1
 
         key = args[2]
@@ -46,6 +53,7 @@ def main(args):
         return 0
     else:
         print "Invalid command argument " + command
+        usage()
         return 1
 
 if __name__ == "__main__":
