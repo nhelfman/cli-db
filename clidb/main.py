f01f9ffc4f6f48a73609de6d94ffbf50b09d95e7
Noam Helfman <noam.helfman@gmail.com>
2015-03-06 19:41:39 -0800

    add command line execution main

--- clidb/main.py
+++ clidb/main.py
@@ -1,10 +1,50 @@
-#! /usr/bin/python
+import sys
+from cli_db import CliDb
 
 
-def main():
-    print "Usage: cli-db [--database my.db.json] <set|get> <value>"
+def main(args):
+    if len(args) < 3:
+        print "Usage: cli-db <set key value|get key|del key>"
 
+    command = args[1]
+    if command == "set":
+        key = args[2]
+        if len(args) < 4:
+            print "missing argument: value"
+            return 1
+
+        value = args[3]
+        db = CliDb()
+        db.set(key, value)
+        return 0
+
+    elif command == "get":
+        if len(args) < 3:
+            print "missing argument: key"
+            return 1
+
+        key = args[2]
+
+        db = CliDb()
+        value = db.get(key)
+        print value
+        return 0
+
+    elif command == "del":
+        if len(args) < 3:
+            print "missing argument: key"
+            return 1
+
+        key = args[2]
+
+        db = CliDb()
+        db.delete(key)
+
+        return 0
+    else:
+        print "Invalid command argument " + command
+        return 1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv))
 
