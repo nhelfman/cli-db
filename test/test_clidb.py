fe4e946e27ae702e2f1e992462caf90cf20baeac
Noam Helfman <noam.helfman@gmail.com>
2015-03-06 19:41:13 -0800

    add more unit test for better coverage

--- test/test_clidb.py
+++ test/test_clidb.py
@@ -1,14 +1,18 @@
 import os
 import tempfile
 import unittest
+import sys
+import clidb
 from clidb.cli_db import CliDb
+import main
 
 
-DB_FILE=os.path.join(tempfile.gettempdir(), "_test_cli-db.json")
+DB_FILE = os.path.join(tempfile.gettempdir(), "_test_cli-db.json")
 
 
 class CliDbTest(unittest.TestCase):
     def setUp(self):
+        self.db = CliDb(DB_FILE)
         if os.path.isfile(DB_FILE):
             os.remove(DB_FILE)
 
@@ -19,11 +23,29 @@ class CliDbTest(unittest.TestCase):
         if os.path.isfile(DB_FILE + ".lock"):
             os.remove(DB_FILE + ".lock")
 
-    def test_1(self):
-        db = CliDb(DB_FILE)
-        db.set("foo", "bar")
+    def test_basic(self):
+        self.db.set("foo", "bar")
 
-        db = CliDb(DB_FILE)
-        actual = db.get("foo")
+        self.db = CliDb(DB_FILE) # new instance
+        actual = self.db.get("foo")
 
         self.assertEquals("bar", actual, "value should be bar")
+
+    def test_no_val(self):
+        val = self.db.get("foo")
+
+        self.assertIsNone(val, "value should not exist")
+
+    def test_delete(self):
+        self.db.set("foo", "bar")
+
+        self.db.delete("foo")
+
+        val = self.db.get("foo")
+        self.assertIsNone(val, "value should be None after delete")
+
+    def test_delete_none_existing(self):
+        self.db.delete("foo")
+
+        val = self.db.get("foo")
+        self.assertIsNone(val, "value should be None after delete")
